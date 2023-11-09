d432ce4264cf5eb4dbb92cc6b7d3f666bee0598c
David Daniel <75741582+daviddaniel2001@users.noreply.github.com>
2023-11-09 19:14:53 -0300

    Validação e Paginação

--- src/main/java/br/luahr/topicos1/resource/FloriculturaResource.java
+++ src/main/java/br/luahr/topicos1/resource/FloriculturaResource.java
@@ -9,16 +9,17 @@ import br.luahr.topicos1.dto.FloriculturaResponseDTO;
 import br.luahr.topicos1.service.FloriculturaService;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
@@ -35,10 +36,15 @@ public class FloriculturaResource {
 
     @GET
     @RolesAllowed({ "Admin", "User" })
-    public List<FloriculturaResponseDTO> getAll() {
-        return floriculturaService.getAll();
+    public List<FloriculturaResponseDTO> getAll(
+            @QueryParam("page") @DefaultValue("o") int page,
+            @QueryParam("pageSize") @DefaultValue("10") int pageSize) {
+        
+        LOG.info("Buscando por todas as floriculturas");
+        LOG.debug("ERRO DE DEBUG");
+        return floriculturaService.getAll(page, pageSize);
     }
-
+    
     @GET
     @Path("/{id}")
     @RolesAllowed({ "Admin", "User" })
@@ -47,27 +53,23 @@ public class FloriculturaResource {
     }
 
     @POST
-    @Transactional
     @RolesAllowed({ "Admin" })
     public Response insert(FloriculturaDTO floriculturaDTO) {
-        try {
-            FloriculturaResponseDTO floricultura = floriculturaService.create(floriculturaDTO);
-            return Response.status(Status.CREATED).entity(floricultura).build();
-        } catch (ConstraintViolationException e) {
-            Result result = new Result(e.getConstraintViolations());
-            return Response.status(Status.NOT_FOUND).entity(result).build();
-        }
+        LOG.infof("Inserindo uma floricultura: %s", floriculturaDTO.nome());
+
+        FloriculturaResponseDTO floricultura = floriculturaService.create(floriculturaDTO);
+        LOG.infof("Floricultura (%d) criada com sucesso.", floricultura.id());
+        return Response.status(Status.CREATED).entity(floricultura).build();
+
+
     }
 
     @PUT
     @Path("/{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    @Transactional
     public Response update(@PathParam("id") Long id, FloriculturaDTO floriculturaDTO) {
         try {
-            floriculturaService.update(id, floriculturaDTO);
-            return Response.status(Status.NO_CONTENT).build();
+            FloriculturaResponseDTO floricultura = floriculturaService.update(id, floriculturaDTO);
+            return Response.ok(floricultura).build();
         } catch (ConstraintViolationException e) {
             Result result = new Result(e.getConstraintViolations());
             return Response.status(Status.NOT_FOUND).entity(result).build();
@@ -81,4 +83,23 @@ public class FloriculturaResource {
         floriculturaService.delete(id);
         return Response.status(Status.NO_CONTENT).build();
     }
+
+    @GET
+    @Path("/count")
+    public long count() {
+        return floriculturaService.count();
+    }
+
+    @GET
+    @Path("/search/{nome}/count")
+    public long count(@PathParam("nome") String nome) {
+        return floriculturaService.countByNome(nome);
+    }
+    
+    public List<FloriculturaResponseDTO> search (
+            @PathParam("nome") String nome,
+            @QueryParam("page") @DefaultValue("0") int page,
+            @QueryParam("pageSize") @DefaultValue("10") int pageSize) {
+        return floriculturaService.findByNome(nome, page, pageSize);
+            }
 }
